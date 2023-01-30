1eec367008679e083ecb5f796b49e8edfccf788f
Abhishek Varma G V S <75376880+varma59@users.noreply.github.com>
2023-01-30 08:55:21 +0530

    Create Minandmaxinarray.py

---
+++ Minandmaxinarray.py
@@ -0,0 +1,13 @@
+def getMinMax( a, n):
+    # max = a[0]
+    # min = a[0]
+    # for i in range(1,n):
+    #     if(a[i])>max:
+    #       max=a[i];
+    #     elif(a[i])<min:
+    #         min=a[i];
+        
+    # maxmin = min ,max;
+    # return maxmin    
+    
+    return min(a),max(a)
